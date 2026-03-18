3ad80f4a8eb7919e77e2c562928ed2bb8f241fa9
Andrey Moskvitin <anselm2007@mail.ru>
2026-03-18 14:09:28 +0300

    Initial commit

---
+++ src/test/java/org/skypro/java/course2/examinerservice/ExaminerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.java.course2.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
